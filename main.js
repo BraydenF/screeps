d54535eb4090994786c8d243c2a2f700b6dbc77a
Brayden Foster <braydenf1616@gmail.com>
2022-04-07 01:15:41 -0500

    Continued work on building out Drone system

--- main.js
+++ main.js
@@ -1,39 +1,18 @@
-const roleService = require('role.service');
-// const consoleService = require('console.service');
+const creepService = require('creep.service');
+const consoleService = require('console.service');
 // const towerService = require('tower.service');
-
-const SPAWN_ZERO_NAME = 'colony_0';
-
-const colonies = [
-    Game.spawns[SPAWN_ZERO_NAME],
-];
-
-// allows for expanding of the game console
-const consoleService = {
-    init: function() {
-        // console commands
-        global.roleService = roleService;
-
-        // short cuts
-        global.createCreep = roleService.createCreep;
-    },
-}
 
 // main logic loop
 module.exports.loop = function () {
+    const spawn = Game.spawns['spawn'];
     console.log('*****************************************************************');
+    console.log('energy:', spawn.room.energyAvailable, '/', spawn.room.energyCapacityAvailable);
     consoleService.init();
 
     function keys(obj) {
         return console.log('keys', Object.keys(obj));
     };
 
-    // const randkey = '624b1b428545bf17e16f179d';
-
-    // keys(Game.structures);
-    // const structures = Game.structures;
-    // console.log(structures[randkey]);
-
     // towerService.run();
-    roleService.run();
+    creepService.run();
 }
---
+++ projects.controller.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+const projectsController = {
+    run: function() {
+    },
+}
+
+module.exports = projectsController;
