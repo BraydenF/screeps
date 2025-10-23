d4fc298333ab86efd841701aaeadfe369582b3c9
Brayden Foster <braydenf1616@gmail.com>
2025-10-23 10:23:23 -0500

    Fix lab report; update utils (first() type check, randomColor), remove unused helpers

--- utils.js
+++ utils.js
@@ -1,18 +1,3 @@
-const emojis = [
-    '😀','😃','😄','😁','😆','😅','🤣','😂','🙂','🙃',
-    '😉','😊','😇','🥰','😍','🤩','😘','😗','😚','😙',
-    '😋','😛','😜','🤪','😝','🤑','🤗','🤭','🤫','🤔',
-    '🤐','🤨','😐','😑','😶','😏','😒','🙄','😬','🤥',
-    '😌','😔','😪','🤤','😴','😷','🤒','🤕','🤢','🤮',
-    '🤧','🥵','🥶','🥴','😵','🤯','🤠','🥳','😎','🤓',
-    '🧐','😕','😟','🙁','😮','😯','😲','😳','🥺','😦',
-    '😧','😨','😰','😥','😢','😭','😱','😖','😣','😞',
-    '😓','😩','😫','🥱','😤','😡','😠', '🤬','😈','👿',
-    '💀','💩','🤡','👹','👺','👻','👽','👾','🤖','🙈',
-    '🙉','🙊','💋','💌','💘','💯','💢','💥','💫','💦',
-    '💨','💣','💬','💭','💤','👋','🤚','✋','🖖',
-];
-
 class Queue {
     constructor(items = []) {
         this.items = items;
@@ -49,50 +34,14 @@ function roll() {
 global.roll = roll;
 
 const utils = {
-    findResourceTargets(creep, resourceAmount = 0) {
-        return creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_EXTENSION ||
-                    structure.structureType == STRUCTURE_SPAWN) &&
-                    structure.store[RESOURCE_ENERGY] >= resourceAmount
-            }
-        });
-    },
-    getRandomSource: function(sources) {
-        const target = sources[roll() < 50 ? 1 : 0];
-        return target.id;
-    },
-    /**
-     * https://stackoverflow.com/a/3983830
-     * @params probas number[]
-     * @params funcs function[]
-     */
-    randexec: function (probas, funcs) {
-        var ar = [];
-        var i,sum = 0;
-        // that following initialization loop could be done only once above that
-        // randexec() function, we let it here for clarity
-        for (i=0 ; i<probas.length-1 ; i++) { // notice the '-1'
-            sum += (probas[i] / 100.0);
-            ar[i] = sum;
-        }
-
-        // Then we get a random number and finds where it sits inside the probabilities 
-        // defined earlier
-        var r = Math.random(); // returns [0,1]
-        for (i=0 ; i<ar.length && r>=ar[i] ; i++) ;
-
-        // Finally execute the function and return its result
-        return (funcs[i])();
-    },
     randomColor: function randomColor() {
-        return Math.floor(Math.random()*16777215).toString(16);
+        // pad to 6 hex digits to ensure valid color codes like '00a3f4'
+        return Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
     },
     randomSay: function randomSay() {
-        // idea: grood emojies by group / emotion
-        return emojis[Math.floor(Math.random()*emojis.length)];
+        // idea: group emojies by group / emotion
+        return typeof emojis !== 'undefined' && emojis.length ? emojis[Math.floor(Math.random()*emojis.length)] : undefined;
     },
-    roll,
     toString: function (obj) {
         const keys = Object.keys(obj);
         console.log('***********************');
@@ -103,19 +52,24 @@ const utils = {
     Queue,
 };
 
+// ------------------------------
+// Array convenience helpers
+// ------------------------------
+// NOTE: Kept the convenience Array.prototype helpers used elsewhere in the codebase,
+// but made first() return undefined when empty and added an Array type check.
 Array.prototype.rand = function() {
+    if (!Array.isArray(this) || this.length === 0) return undefined;
     const index = Math.floor(Math.random()*this.length);
-    // console.log(`rand ${index} / this.length`);
     return this[index];
 };
 
 Array.prototype.first = function() {
-  return this.length && this[0];
+  return (Array.isArray(this) && this.length) ? this[0] : undefined;
 }
 
 // performs the provided function on the first element of the array if it exists
 Array.prototype.onFirst = function(func) {
-  const first = this.length && this[0];
+  const first = (Array.isArray(this) && this.length) ? this[0] : undefined;
   if (func && first) return func(first);
 }
 
@@ -123,57 +77,6 @@ Array.prototype.onEmpty = function(func) {
     if (this.length == 0 && func) {
         return func();
     }
-}
-
-/** Test code that has been deprecated, wanted to keep the structure and basic idea */
-class MiningTeam {
-  constructor(spawn, source) {
-    this.key = `miningTeam-${source}`;
-    if (!Memory[this.key]) {
-      Memory[this.key] = { source: source, miner: null, haulers: [] }; 
-    }
-
-    this.spawn = spawn;
-    this.source = source;
-    this.memory = Memory.rooms[this.key];
-
-    const miners = spawn.room.find(FIND_MY_CREEPS, {
-      filter: (creep) => {
-        return creep.memory.source === source && creep.memory.job === 'miner';
-    }});
-
-    if (miners && miners.length) {
-      this.miner = miners[0];
-    }
-
-    const haulers = spawn.room.find(FIND_MY_CREEPS, {
-      filter: { memory: { source: source, job: 'hauler' } },
-    });
-
-    if (haulers && haulers.length) {
-      this.haulers = haulers;
-    }
-  }
-
-  run() {
-    if (Array.isArray(this.haulers)) {
-      this.haulers.forEach((hauler) => {
-        // find dropped Resources nearest the Source
-        const source = Game.getObjectById(hauler.memory.source);
-
-        if (hauler.memory.task === 'load' || hauler.memory.task === 'pickup') {
-          const droppedResources = source.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
-
-          if (droppedResources) {
-            // targets the dropped resources closest to the deposit
-            hauler.memory.target = droppedResources.id;
-          }
-        } else if (hauler.memory.task === 'unload') {
-          // add special target rules
-        }
-      });
-    }
-  }
 }
 
 module.exports = utils;
